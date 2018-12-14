da2a983f6f9aa219add3a6a557e7f43e86adadfc
hugopinto1997 <hugopillo@hotmail.com>
2018-12-14 15:15:41 -0600

    Login Screen

--- app/src/main/java/com/example/max00/republica_sazon/Activities/SplashScreen.java
+++ app/src/main/java/com/example/max00/republica_sazon/Activities/SplashScreen.java
@@ -3,17 +3,24 @@ package com.example.max00.republica_sazon.Activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.TransitionManager;
+import android.util.Log;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.example.max00.republica_sazon.R;
 
 public class SplashScreen extends AppCompatActivity {
 
+    private ViewGroup splash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        splash = findViewById(R.id.splash);
 
         //Thread para cambio de pantalla
         Thread timer= new Thread()
@@ -32,7 +39,8 @@ public class SplashScreen extends AppCompatActivity {
                 }
                 finally
                 {
-                    Intent Startup = new Intent(getApplicationContext(), MainActivity.class);
+                    Intent Startup = new Intent(getApplicationContext(), Login.class);
+                    TransitionManager.beginDelayedTransition(splash);
                     startActivity(Startup);
                 }
             }
