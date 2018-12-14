85677af725828683007bdfcce6e8c0f7b3608a1a
maxisun <00354615@uca.edu.sv>
2018-12-14 15:02:50 -0600

    firebase dependencies

--- app/src/main/java/com/example/max00/republica_sazon/Activities/SplashScreen.java
+++ app/src/main/java/com/example/max00/republica_sazon/Activities/SplashScreen.java
@@ -9,45 +9,35 @@ import com.example.max00.republica_sazon.R;
 
 public class SplashScreen extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
         //Thread para cambio de pantalla
-        Thread timer= new Thread()
-        {
-            public void run()
-            {
-                try
-                {
+        Thread timer= new Thread(){
+            public void run(){
+                try{
                     //Display for 3 seconds
                     sleep(3000);
                 }
-                catch (InterruptedException e)
-                 {
+                catch (InterruptedException e){
                     // TODO: handle exception
                     e.printStackTrace();
                 }
-                finally
-                {
+                finally{
                     Intent Startup = new Intent(getApplicationContext(), MainActivity.class);
                     startActivity(Startup);
                 }
             }
         };
         timer.start();
-
-
     }
 
     @Override
-    protected void onPause()
-    {
+    protected void onPause(){
         super.onPause();
         finish();
-
     }
 }
 
